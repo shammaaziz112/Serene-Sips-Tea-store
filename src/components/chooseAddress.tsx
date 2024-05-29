ae3199cbd05774c1802210b479a2c7f5a033cab7
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-05-29 03:17:54 +0300

    update address

--- src/components/chooseAddress.tsx
+++ src/components/chooseAddress.tsx
@@ -2,12 +2,12 @@ import { Address } from "@/types"
 import addressService from "../api/addresses"
 import { useQuery } from "@tanstack/react-query"
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue
-  } from "@/components/ui/select"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select"
 
 export function ChooseAddres() {
   const { data: addresses } = useQuery<Address[]>({
@@ -16,9 +16,9 @@ export function ChooseAddres() {
   })
   return (
     <div className="grow mr-3">
-      <Select >
+      <Select>
         <SelectTrigger className="col-span-3" name="categoryId">
-          <SelectValue placeholder="Addres" />
+          <SelectValue placeholder="Address" />
         </SelectTrigger>
         <SelectContent>
           {addresses?.map((address: Address) => {
